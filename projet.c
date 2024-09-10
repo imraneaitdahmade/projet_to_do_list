1e4d3a9923cbd85a6710532bdbe66018fe6b6a8e
imraneaitdahmade <imraneaitdahmade@gmail.com>
2024-09-10 09:25:08 +0100

    projt

---
+++ projet.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main(){
+    //*declaration des variables 
+    int x;
+    //* 1 |menu principal*// 
+    printf(" __________________________________________________________________________\n");
+    printf(" _________________________________  to do list ____________________________\n");
+    printf(" 1)ajouter des taches                           2)affichage des taches      \n \n ");
+    printf("3)trier les taches                             4)recherche des taches      \n \n ");
+    printf("5)modification des taches                       6)la suppression  des taches\n\n");
+    printf(" __________________________________________________________________________\n");
+    printf(" __________________________________________________________________________\n");
+    //*finir le menu principal
+
+    printf("boujour user entre le numero correspond a le service \n");
+    printf("entre :");
+    scanf("%d",&x);
+
+    switch(x){
+case 1:
+break;
+case 2:
+break;
+case 3:
+break;
+case 4:
+break;
+case 5:
+break;
+case 6:
+break;
+    }
+}
